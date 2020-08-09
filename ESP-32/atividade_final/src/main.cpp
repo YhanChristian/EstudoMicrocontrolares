45c64629e5e39e45267e7cc850a7170c9e1c3e20
yhanchristian <yhan.christian@live.com>
2020-08-09 15:18:39 -0300

    mudancas projeto

---
+++ ESP-32/atividade_final/src/main.cpp
@@ -0,0 +1,16 @@
+/* Exemplo de aplicação prática com FreeRTOS e ESP32
+   Realiza a leitura de um sensor (LM35), imprime valor no display OLED
+   publica valor a cada 30s no broker MQTT.
+   Dados sendo acompanhados no Tago IO
+*/
+
+
+#include "main.h"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}

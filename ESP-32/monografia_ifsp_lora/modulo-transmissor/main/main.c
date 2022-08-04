f50799ad0c10b3f2a7d0bf1b86a841552d93eca3
Yhan Christian <yhan.christian@live.com>
2022-08-03 21:58:12 -0300

    Inicio projeto - modulo transmissor

---
+++ ESP-32/monografia_ifsp_lora/modulo-transmissor/main/main.c
@@ -0,0 +1,22 @@
+/**
+ ******************************************************************************
+ * @Company    : Yhan Christian Souza Silva
+ * @file       : main.c
+ * @author     : Yhan Christian Souza Silva
+ * @date       : 03/08/2022
+ * @brief      : Arquivo fonte main.c com o projeto do transmissor (MASTER) que
+ *               envia um comando ao módulo SLAVE para receber dados do sensor
+ *               acelerômetro (MPU6050) com os seguintes parâmetros calculados
+ *               - Velocidade (RMS)
+ *               - Aceleração (RMS e Pico)
+ *               via LoRa em uma comunicação PaP.
+ ******************************************************************************
+ */
+
+
+#include <stdio.h>
+
+void app_main(void)
+{
+
+}

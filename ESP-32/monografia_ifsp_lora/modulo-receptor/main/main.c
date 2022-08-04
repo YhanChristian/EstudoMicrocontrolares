7f623691f0c4d30b78712d9537743dfa01dcdebc
Yhan Christian <yhan.christian@live.com>
2022-08-03 21:53:43 -0300

    Inicio projeto - modulo receptor

---
+++ ESP-32/monografia_ifsp_lora/modulo-receptor/main/main.c
@@ -0,0 +1,24 @@
+/**
+ ******************************************************************************
+ * @Company    : Yhan Christian Souza Silva
+ * @file       : main.c
+ * @author     : Yhan Christian Souza Silva
+ * @date       :03/08/2022
+ * @brief      : Arquivo fonte main.c com o projeto do receptor (SLAVE) que
+ *               aguarda comando do MASTER para envio dos dados lidos por um
+ *               acelerômetro realiza o cálculo dos sguintes parâmetros:
+ *                 - Velocidade (RMS)
+ *                 - Aceleração (RMS e Pico)
+ * 
+ *              E por fim,  envia LoRA em uma comunicação PaP.
+ ******************************************************************************
+ */
+
+
+
+#include <stdio.h>
+
+void app_main(void)
+{
+
+}

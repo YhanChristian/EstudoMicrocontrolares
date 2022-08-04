eb9485ac3c4425ba94762233cb0fe5d5bdfbf55a
Yhan Christian <yhan.christian@live.com>
2022-08-03 22:23:28 -0300

    Inclusão - MPU6050 Component

--- ESP-32/monografia_ifsp_lora/modulo-receptor/main/main.c
+++ ESP-32/monografia_ifsp_lora/modulo-receptor/main/main.c
@@ -14,9 +14,47 @@
  ******************************************************************************
  */
 
-
+/* Includes ------------------------------------------------------------------*/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+
+#include "nvs_flash.h"
+#include "esp_spi_flash.h"
+
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_log.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+
+#include "mpu6050.h"
+
+
+/* Private define & constants ------------------------------------------------*/
+
+static const char *TAG = "LoRa_Receiver";
+
+/**
+ * LoRa device Address;
+ */
+const int MASTER_NODE_ADDRESS = 0;
+
+/**
+ * ATENÇÃO:
+ * Defina aqui o endereço deste DISPOSITIVO; 1 a 255;
+ * Cada dispositivo SLAVE precisa ter endereço diferente;
+ */
+const int SLAVE_NODE_ADDRESS = 1;
+
+#define CMD_READ_MPU6050 1
+
+
 
 void app_main(void)
 {

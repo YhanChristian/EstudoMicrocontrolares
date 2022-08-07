1ee0c118707f08e17c4d2bfe2b44e2f7d983e1dd
Yhan Christian <yhan.christian@live.com>
2022-08-07 16:16:23 -0300

    Inclusão de bibliotecas projeto (LoRa e Display)

--- ESP-32/monografia_ifsp_lora/modulo-receptor/main/main.c
+++ ESP-32/monografia_ifsp_lora/modulo-receptor/main/main.c
@@ -9,7 +9,7 @@
  *               acelerômetro realiza o cálculo dos sguintes parâmetros:
  *                 - Velocidade (RMS)
  *                 - Aceleração (RMS e Pico)
- * 
+ *
  *              E por fim,  envia LoRA em uma comunicação PaP.
  ******************************************************************************
  */
@@ -34,7 +34,7 @@
 #include "freertos/semphr.h"
 
 #include "mpu6050.h"
-
+#include "lora.h"
 
 /* Private define & constants ------------------------------------------------*/
 
@@ -54,9 +54,70 @@ const int SLAVE_NODE_ADDRESS = 1;
 
 #define CMD_READ_MPU6050 1
 
+/* I2C Interface inicialization ----------------------------------------------*/
 
+/*!< Config I²C Display OLED */
+#define I2C_SDA_PIN CONFIG_I2C_SDA_PIN       /*!< default 4 */
+#define I2C_SCL_PIN CONFIG_I2C_SCL_PIN       /*!< default 15 */
+#define I2C_CHANNEL CONFIG_I2C_CHANNEL       /*!< default 0 */
+#define OLED_PIN_RESET CONFIG_OLED_PIN_RESET /*!< default 16 */
+
+/*!< Config I²C MPU6050 */
+#define I2C_MASTER_SCL_IO 22      /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO 21      /*!< gpio number for I2C master data  */
+#define I2C_MASTER_NUM I2C_NUM_1  /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ 100000 /*!< I2C master clock frequency */
+
+mpu6050_handle_t mpu6050 = NULL;
+
+/* Private typedef -----------------------------------------------------------*/
+
+mpu6050_acce_value_t MPU6050_Acce_Data;
+mpu6050_temp_value_t MPU6050_Temp_Data;
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private tasks prototypes --------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+
+static void esp32_start(void);
+
+/* app_main function body ----------------------------------------------------*/
 
 void app_main(void)
 {
+    /*!< Inicia ESP32 e exibe algumas informações */
+    esp32_start();
+}
 
+/* Bodies of private tasks ---------------------------------------------------*/
+
+/* Bodies of private functions -----------------------------------------------*/
+
+static void esp32_start(void)
+{
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
+           chip_info.cores,
+           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+    printf("silicon revision %d, ", chip_info.revision);
+
+    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    // Initialize NVS
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        // NVS partition was truncated and needs to be erased
+        // Retry nvs_flash_init
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
 }

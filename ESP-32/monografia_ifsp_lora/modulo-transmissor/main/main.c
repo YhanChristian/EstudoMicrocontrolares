dd798de261e4762a1c0caca0e4775a1cb7ed91c3
Yhan Christian <yhan.christian@live.com>
2022-09-08 22:12:02 -0300

    Inicialização transmissor e Tasks

--- ESP-32/monografia_ifsp_lora/modulo-transmissor/main/main.c
+++ ESP-32/monografia_ifsp_lora/modulo-transmissor/main/main.c
@@ -76,17 +76,90 @@ const int MASTER_NODE_ADDRESS = 0;
 
 /* Global variables -----------------------------------------------------------*/
 
+char str_ip[16];
+
+static const char *MQTT_TOPIC = "ifsp/monografia-lora";
+
 /* FreeRTOS section - Handles and tasks prototypes ----------------------------*/
+
+static void vLoRaTxTask(void *pvParameter);
+static void vMQTTPublishTask(void *pvParameter);
+
+static EventGroupHandle_t wifi_connected_event_group;
+const static int WIFI_CONNECTED = BIT0;
+
+QueueHandle_t sensor_data_queue;
+SemaphoreHandle_t mqtt_connected_mutex;
+
+extern esp_mqtt_client_handle_t client;
 
 /* Private function prototypes -----------------------------------------------*/
 
 static void esp32_start(void);
+static void ssd1306_start(void);
+static void lora_received_data(void);
+void cb_connection_ok(void *pvParameter);
+
+/* app_main function body ----------------------------------------------------*/
 
 void app_main(void)
 {
     /*!< Inicia ESP32 e exibe algumas informações */
     esp32_start();
+
+    /*!< Inicia display  OLED 0.96"*/
+    ssd1306_start();
+
+    /*!< Cria semaforo Mutex para sinalizar envio dados MQTT*/
+    mqtt_connected_mutex = xSemaphoreCreateMutex();
+
+    if (mqtt_connected_mutex == NULL)
+    {
+        ESP_LOGE("ERROR", "*** mqtt_connected_mutex Create error ***\n");
+    }
+
+    /*!< Cria fila para armazenamento dos dados lidos sensor*/
+    sensor_data_queue = xQueueCreate(10, sizeof(char *));
+
+    if (sensor_data_queue == NULL)
+    {
+        ESP_LOGE("ERROR", "*** sensor_data_queue Create error ***\n");
+    }
+
+    /*!< Inicia conexão WiFi manager */
+    wifi_manager_start();
+
+    /*!< register a callback as an example to how you can integrate your code with the wifi manager */
+    wifi_manager_set_callback(WM_EVENT_STA_GOT_IP, &cb_connection_ok);
+
+    /**
+     * Aguarda conexão WiFi para processar Task
+     */
+    xEventGroupWaitBits(wifi_connected_event_group, WIFI_CONNECTED, false, true, portMAX_DELAY);
+
+    /*!< Inicia LoRa e seta frequência 915MHz*/
+    lora_init();
+    lora_set_frequency(915E6);
+
+    /*!< Habilita CRC*/
+    lora_enable_crc();
+    /*!< Habilita a recepção LoRa via Interrupção Externa;*/
+    lora_enable_irq();
 }
+
+/* Bodies of private tasks ---------------------------------------------------*/
+
+static void vLoRaTxTask(void *pvParameter)
+{
+    for (;;)
+}
+
+static void vMQTTPublishTask(void *pvParameter)
+{
+    for (;;)
+}
+
+/* Bodies of private functions -----------------------------------------------*/
 
 static void esp32_start(void)
 {
@@ -113,4 +186,31 @@ static void esp32_start(void)
         err = nvs_flash_init();
     }
     ESP_ERROR_CHECK(err);
+}
+
+static void ssd1306_start(void)
+{
+    ssd1306_config(I2C_SDA_PIN, I2C_SCL_PIN, I2C_CHANNEL, OLED_PIN_RESET);
+    ssd1306_out16(0, 0, "Trans. Addr:", WHITE);
+    ssd1306_chr16(0, 13, MASTER_NODE_ADDRESS + '0', WHITE);
+    ssd1306_out8(3, 2, "Waiting WiFi", WHITE);
+    ssd1306_out8(4, 3, "Connection", WHITE);
+}
+
+static void lora_received_data(void)
+{
+}
+
+void cb_connection_ok(void *pvParameter)
+{
+    xEventGroupSetBits(wifi_connected_event_group, WIFI_CONNECTED);
+    ip_event_got_ip_t *param = (ip_event_got_ip_t *)pvParameter;
+    /* transform IP to human readable string */
+    esp_ip4addr_ntoa(&param->ip_info.ip, str_ip, IP4ADDR_STRLEN_MAX);
+    ESP_LOGI(TAG, "I have a connection and my IP is %s!", str_ip);
+    ssd1306_clear();
+    ssd1306_out16(0, 0, "Trans. Addr:", WHITE);
+    ssd1306_chr16(0, 13, MASTER_NODE_ADDRESS + '0', WHITE);
+    ssd1306_out8(3, 0, "IP: ", WHITE);
+    ssd1306_out8(3, 3, str_ip, WHITE);
 }

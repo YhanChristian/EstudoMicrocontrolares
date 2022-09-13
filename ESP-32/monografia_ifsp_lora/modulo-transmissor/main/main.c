b545f796dc9a7afc56c88271445e79eeceb2ec27
Yhan Christian <yhan.christian@live.com>
2022-09-12 23:14:46 -0300

    Módulo transmissor - tratamento msgs e rotina LoRa

--- ESP-32/monografia_ifsp_lora/modulo-transmissor/main/main.c
+++ ESP-32/monografia_ifsp_lora/modulo-transmissor/main/main.c
@@ -28,6 +28,9 @@
 #include "esp_event.h"
 #include "esp_log.h"
 
+#include <esp_wifi.h>
+#include <esp_netif.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
@@ -40,6 +43,7 @@
 #include "wifi_manager.h"
 #include "esp32_mqtt.h"
 #include "mqtt_client.h"
+#include "cJSON.h"
 
 /* Private define & constants ------------------------------------------------*/
 
@@ -59,6 +63,8 @@ const int MASTER_NODE_ADDRESS = 0;
 #define LORA_RECEIVER_TIMEOUT_MS 5000
 
 #define CMD_READ_MPU6050 1
+
+#define BIT_0 (1 << 0)
 
 /* I2C Interface inicialization ----------------------------------------------*/
 
@@ -85,10 +91,11 @@ static const char *MQTT_TOPIC = "ifsp/monografia-lora";
 static void vLoRaTxTask(void *pvParameter);
 static void vMQTTPublishTask(void *pvParameter);
 
-static EventGroupHandle_t wifi_connected_event_group;
-const static int WIFI_CONNECTED = BIT0;
+EventGroupHandle_t wifi_connected_event_group;
+const static int WIFI_CONNECTED = BIT_0;
 
 QueueHandle_t sensor_data_queue;
+QueueHandle_t count_pkg_queue;
 SemaphoreHandle_t mqtt_connected_mutex;
 
 extern esp_mqtt_client_handle_t client;
@@ -99,7 +106,8 @@ static void esp32_start(void);
 static void ssd1306_start(void);
 static void lora_received_data(void);
 void cb_connection_ok(void *pvParameter);
-
+void disp_connected(void);
+void disp_packages(uint32_t uiPckTransceived, uint32_t uiPckReceived);
 /* app_main function body ----------------------------------------------------*/
 
 void app_main(void)
@@ -109,6 +117,15 @@ void app_main(void)
 
     /*!< Inicia display  OLED 0.96"*/
     ssd1306_start();
+
+    /*!< Cria grupo de evento para sinalizar conexão WiFi */
+    wifi_connected_event_group = xEventGroupCreate();
+    if (wifi_connected_event_group == NULL)
+    {
+        ESP_LOGE("ERROR", "*** wifi_connected_event_group Create error ***\n");
+        while (true)
+            ;
+    }
 
     /*!< Cria semaforo Mutex para sinalizar envio dados MQTT*/
     mqtt_connected_mutex = xSemaphoreCreateMutex();
@@ -124,6 +141,14 @@ void app_main(void)
     if (sensor_data_queue == NULL)
     {
         ESP_LOGE("ERROR", "*** sensor_data_queue Create error ***\n");
+    }
+
+    /*!< Cria fila para contador de pacotes enviados*/
+    count_pkg_queue = xQueueCreate(5, sizeof(uint32_t));
+
+    if (count_pkg_queue == NULL)
+    {
+        ESP_LOGE("ERROR", "*** count_pkg_queue Create error ***\n");
     }
 
     /*!< Inicia conexão WiFi manager */
@@ -143,7 +168,6 @@ void app_main(void)
 
     /*!< Habilita CRC*/
     lora_enable_crc();
-    
     /*!< Habilita a recepção LoRa via Interrupção Externa;*/
     lora_enable_irq();
 
@@ -166,12 +190,74 @@ void app_main(void)
 
 static void vLoRaTxTask(void *pvParameter)
 {
+    uint8_t protocol[100];
+    static uint32_t count = 0;
     for (;;)
+    {
+        if (xSemaphoreTake(mqtt_connected_mutex, portMAX_DELAY))
+        {
+            /**
+             * Protocolo;
+             * <id_node_sender><id_node_receiver><command><payload_size><payload><crc>
+             */
+            protocol[0] = MASTER_NODE_ADDRESS;
+            protocol[1] = LORA_TOTAL_NODES;
+            protocol[2] = CMD_READ_MPU6050;
+            protocol[3] = ++count;
+            /**
+             * Calcula o CRC do pacote;
+             */
+            USHORT usCRC = usLORACRC16(protocol, 4);
+            protocol[4] = (UCHAR)(usCRC & 0xFF);
+            protocol[5] = (UCHAR)((usCRC >> 8) & 0xFF);
+
+            /**
+             * Transmite protocol via LoRa;
+             */
+            lora_send_packet(protocol, 6);
+
+            ESP_LOGI(TAG, "Pacote = %d | Enviado para node = %d", count, LORA_TOTAL_NODES);
+
+            if (xQueueSend(count_pkg_queue, &count, (10 / portTICK_RATE_MS) == pdPASS))
+            {
+                ESP_LOGI(TAG, "Envia dados fila: %d", count);
+            }
+
+            /**
+             * Atualiza Display informando o pacote enviado
+             *
+             */
+            ssd1306_clear();
+            disp_connected();
+            disp_packages(count, -1);
+
+            vTaskDelay(10 / portTICK_RATE_MS);
+
+            /**
+             * Chama a função que irá receber os dados do acelerômetro, enviado pelo receptor;
+             */
+            lora_received_data();
+
+            xSemaphoreGive(mqtt_connected_mutex);
+            vTaskDelay(10E3 / portTICK_RATE_MS);
+        }
+    }
 }
 
 static void vMQTTPublishTask(void *pvParameter)
 {
+    /*!< Inicio MQTT configurando Broker, User, ETC*/
+    mqtt_app_start();
+
     for (;;)
+    {
+        if (xSemaphoreTake(mqtt_connected_mutex, portMAX_DELAY))
+        {
+            /* Em desenvolvimento função */
+        }
+        xSemaphoreGive(mqtt_connected_mutex);
+        vTaskDelay(10 / portTICK_RATE_MS);
+    }
 }
 
 /* Bodies of private functions -----------------------------------------------*/
@@ -214,6 +300,78 @@ static void ssd1306_start(void)
 
 static void lora_received_data(void)
 {
+    int x;
+    int count = 0;
+    uint32_t transceiverCount = 0;
+    uint32_t receiverCount = 0;
+    uint8_t protocol[150];
+
+    if (xQueueReceive(count_pkg_queue, &transceiverCount, 0) == pdPASS)
+    {
+        ESP_LOGI(TAG, "Pacotes enviado Transceiver: %d", transceiverCount);
+    }
+
+    if (xQueueReceive(xQueue_LoRa, &count, LORA_RECEIVER_TIMEOUT_MS / portTICK_PERIOD_MS) == pdTRUE)
+    {
+
+        while (lora_received())
+        {
+            x = lora_receive_packet(protocol, sizeof(protocol));
+            /**
+             * Protocolo;
+             * <id_node_sender><id_node_receiver><command><payload_size><payload><crc>
+             */
+            if (x >= 6 && protocol[1] == MASTER_NODE_ADDRESS)
+            {
+                /**
+                 * Verifica CRC;
+                 */
+                USHORT usCRC = usLORACRC16(protocol, 3 + protocol[3] + 1);
+                UCHAR ucLow = (UCHAR)(usCRC & 0xFF);
+                UCHAR ucHigh = (UCHAR)((usCRC >> 8) & 0xFF);
+
+                if (ucLow == protocol[3 + protocol[3] + 1] && ucHigh == protocol[3 + protocol[3] + 2])
+                {
+                    ESP_LOGI(TAG, "CRC OK!");
+                    switch (protocol[2])
+                    {
+                    case CMD_READ_MPU6050:
+
+                        ESP_LOGI(TAG, "Dados recebidos MPU6050 - Receiver: %d", LORA_TOTAL_NODES);
+
+                        // ESP_LOGI(TAG, "Dados recebidos MPU6050 - Receiver: %d Data: %s\n\n", LORA_TOTAL_NODES,(char *)&protocol[4]);
+
+                        ssd1306_clear();
+                        disp_connected();
+                        ssd1306_out8(5, 0, "ACK: 0", WHITE);
+                        ssd1306_out8(5, 8, "Rec.: ", WHITE);
+                        ssd1306_chr8(5, 13, LORA_TOTAL_NODES + '0', WHITE);
+                        disp_packages(transceiverCount, receiverCount);
+                        break;
+                    }
+                }
+
+                else
+                {
+                    ESP_LOGI(TAG, "CRC ERROR!");
+                    ssd1306_clear();
+                    disp_connected();
+                    ssd1306_out8(5, 0, "CRC Error", WHITE);
+                    disp_packages(transceiverCount, receiverCount);
+                }
+            }
+        }
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+    else
+    {
+        ESP_LOGI(TAG, "timeout!");
+        ssd1306_clear();
+        disp_connected();
+        ssd1306_out8(5, 0, "ACK: -1", WHITE);
+        ssd1306_out8(5, 8, "Timeout", WHITE);
+        disp_packages(transceiverCount, receiverCount);
+    }
 }
 
 void cb_connection_ok(void *pvParameter)
@@ -224,8 +382,34 @@ void cb_connection_ok(void *pvParameter)
     esp_ip4addr_ntoa(&param->ip_info.ip, str_ip, IP4ADDR_STRLEN_MAX);
     ESP_LOGI(TAG, "I have a connection and my IP is %s!", str_ip);
     ssd1306_clear();
+    disp_connected();
+}
+
+void disp_connected(void)
+{
     ssd1306_out16(0, 0, "Trans. Addr:", WHITE);
     ssd1306_chr16(0, 13, MASTER_NODE_ADDRESS + '0', WHITE);
     ssd1306_out8(3, 0, "IP: ", WHITE);
     ssd1306_out8(3, 3, str_ip, WHITE);
+    ssd1306_out8(3, 3, str_ip, WHITE);
+}
+
+void disp_packages(uint32_t uiPckTransceived, uint32_t uiPckReceived)
+{
+    char cBufTransceived[16], cBufReceived[16];
+    sprintf(cBufTransceived, "%d", uiPckTransceived);
+    ssd1306_out8(7, 0, "pE: ", WHITE);
+    ssd1306_out8(7, 3, cBufTransceived, WHITE);
+
+    if (uiPckReceived == -1)
+    {
+        ssd1306_out8(7, 7, "pR: ", WHITE);
+        ssd1306_chr8(7, 10, '-', WHITE);
+    }
+    else
+    {
+        sprintf(cBufReceived, "%d", uiPckReceived);
+        ssd1306_out8(7, 7, "pR: ", WHITE);
+        ssd1306_out8(7, 10, cBufReceived, WHITE);
+    }
 }
